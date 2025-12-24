b4d7849e282b901cf6ae8e7dd9cfc95588d6a1ef
Randy Pahang II <rpahang2@gmail.com>
2025-12-23 22:45:49 -0800

    Refactoring

--- website-generator-app/frontend/src/app/api/portfolio/[id]/delete/route.ts
+++ website-generator-app/frontend/src/app/api/portfolio/[id]/delete/route.ts
@@ -1,17 +1,11 @@
-import { adminSupabase } from "@/utils/supabase/admin";
 import { createServerSupabaseClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
-export async function DELETE(
-   req: Request,
-   context: { params: Promise<{ id: string }> }
-) {
-    
-    // Retrieve portfolio id from url 
+export async function DELETE(context: { params: Promise<{ id: string }> }) {
+    // Retrieve portfolio id from url
     const { id: portfolioId } = await context.params;
 
-   try {
-          
+    try {
         const supabase = await createServerSupabaseClient();
         const {
             data: { user },
@@ -22,35 +16,31 @@ export async function DELETE(
         if (authError || !user) {
             return NextResponse.json(
                 { error: "Unauthorized access!" },
-                { status: 401 }
+                { status: 401 },
             );
-        } 
+        }
 
         // Delete portfolio row if owned by user
         const { error } = await supabase
             .from("portfolios")
             .delete()
             .eq("id", portfolioId)
-            .eq("user_id", user.id)
-
+            .eq("user_id", user.id);
 
         // Check for error
         if (error) {
             return NextResponse.json(
                 { error: "Failed to delete portfolio!" },
-                { status: 403 }
+                { status: 403 },
             );
         }
 
-
         return NextResponse.json({ success: true });
-            
     } catch (err) {
         console.error("Server error:", err);
         return NextResponse.json(
             { error: "Unexpected server error" },
-            { status: 500 }
+            { status: 500 },
         );
     }
-
 }
