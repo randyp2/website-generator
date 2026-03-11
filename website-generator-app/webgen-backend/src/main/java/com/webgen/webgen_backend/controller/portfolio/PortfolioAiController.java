1780dbfbdc2371c62e0c21dcb8857a3cadadb231
Randy Pahang II <rpahang2@gmail.com>
2026-03-11 00:52:59 -0700

    add generateJobService

--- website-generator-app/webgen-backend/src/main/java/com/webgen/webgen_backend/controller/portfolio/PortfolioAiController.java
+++ website-generator-app/webgen-backend/src/main/java/com/webgen/webgen_backend/controller/portfolio/PortfolioAiController.java
@@ -22,6 +22,7 @@ import java.util.UUID;
 public class PortfolioAiController {
 
     private final PortfolioAiService portfolioAiService;
+    
 
     @PostMapping("/{id}/generate")
     public ResponseEntity<?> generatePortfolio(
