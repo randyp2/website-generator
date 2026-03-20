10374c895c077a9d1b143eb524087311d75ba2b9
Randy Pahang II <rpahang2@gmail.com>
2026-03-19 21:59:37 -0700

    add rabbitmq config

---
+++ website-generator-app/webgen-backend/src/main/java/com/webgen/webgen_backend/config/RabbitMQConfig.java
@@ -0,0 +1,75 @@
+package com.webgen.webgen_backend.config;
+
+
+import ch.qos.logback.classic.pattern.MessageConverter;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.MessageCondition;
+
+@Configuration
+public class RabbitMQConfig {
+
+    // Exchange (interfaces with work queue)
+    public static final String EXCHANGE = "portfolio.exchange";
+    // Work QUEUE
+    public static final String QUEUE = "portfolio.generate.queue";
+    // Label on where to send messages
+    public static final String ROUTING_KEY = "portfolio.generate";
+
+    // Exchange for DLQ
+    public static final String DLX = "portfolio.dlx";
+    // Dead letter queue for retries/inspections
+    public static final String DLQ = "portfolio.generate.dlq";
+    public static final String DLQ_ROUTING_KEY = "portfolio.generate.dead";
+
+
+    @Bean
+    public Queue generateQueue() {
+        // - Generate persistent/durable queue
+        // - Send to dead letter queue if message fails
+        // - Send to DLQ w/ specified routing key
+        return QueueBuilder.durable(QUEUE)
+                .withArgument("x-dead-letter-exchange", DLX)
+                .withArgument("x-dead-letter-routing-key", DLQ_ROUTING_KEY)
+                .build();
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE);
+    }
+
+
+    // Connect queue to portfolio.exchange w/ specified routing key
+    // Send messages w/ routing key to portfolio.exchange
+    @Bean
+    public Binding binding(Queue generateQueue, DirectExchange exchange) {
+        return BindingBuilder.bind(generateQueue).to(exchange).with(ROUTING_KEY);
+    }
+
+    // Configure dead letter queue
+    @Bean
+    public Queue deadLetterQueue() {
+        return QueueBuilder.durable(DLQ).build();
+    }
+
+    @Bean
+    public DirectExchange deadLetterExchange() {
+        return new DirectExchange(DLX);
+    }
+
+    @Bean
+    public Binding dlqBinding(Queue deadLetterQueue, DirectExchange deadLetterExchange) {
+        return BindingBuilder.bind(deadLetterQueue).to(deadLetterExchange).with(DLQ_ROUTING_KEY);
+    }
+
+    // Serialize messages to JSON
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+}
