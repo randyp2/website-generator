8e2e98e4392194eded2fdb0094e47ce38bf4cb56
Randy Pahang II <rpahang2@gmail.com>
2026-03-20 13:20:14 -0700

    chore: final clean up

--- website-generator-app/webgen-backend/src/main/java/com/webgen/webgen_backend/dto/portfolio/PortfolioGenerateResponseDTO.java
+++ website-generator-app/webgen-backend/src/main/java/com/webgen/webgen_backend/dto/portfolio/PortfolioGenerateResponseDTO.java
@@ -2,11 +2,13 @@ package com.webgen.webgen_backend.dto.portfolio;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PortfolioGenerateResponseDTO {
     String previewUrl;
     List<SectionDTO> sections;
