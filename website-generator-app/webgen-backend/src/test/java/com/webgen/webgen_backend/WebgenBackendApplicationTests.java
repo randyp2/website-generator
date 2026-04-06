54a84c46a45b57ffdc7ba5fa6dcf93624fc46c30
Randy Pahang II <rpahang2@gmail.com>
2026-04-05 20:55:54 -0700

    feat: add application test properties for testing

--- website-generator-app/webgen-backend/src/test/java/com/webgen/webgen_backend/WebgenBackendApplicationTests.java
+++ website-generator-app/webgen-backend/src/test/java/com/webgen/webgen_backend/WebgenBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.webgen.webgen_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WebgenBackendApplicationTests {
 
 	@Test
