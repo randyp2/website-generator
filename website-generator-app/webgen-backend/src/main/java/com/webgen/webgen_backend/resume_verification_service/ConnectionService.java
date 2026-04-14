478051fd04b81dfe67ee6be6e398b680ce543dcb
Randy Pahang II <rpahang2@gmail.com>
2026-04-14 10:02:30 -0700

    chore: comments on interface methods

--- website-generator-app/webgen-backend/src/main/java/com/webgen/webgen_backend/resume_verification_service/ConnectionService.java
+++ website-generator-app/webgen-backend/src/main/java/com/webgen/webgen_backend/resume_verification_service/ConnectionService.java
@@ -6,5 +6,17 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ConnectionService {
+
+
+    /**
+     * Return the list of connected accounts for a specific user
+     *  - Sort in descending order based on when the accounts were created
+     * @param profileId - UUID of the profile to search for connected accounts
+     * @return a List of ConnectedAccountDTO's of the given profileID
+     */
     List<ConnectedAccountDTO> getConnections(UUID profileId);
+
+
+    
+
 }
