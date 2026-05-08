edd193880882be888688382fa4e2bdb8c3e14ff7
Randy Pahang II <rpahang2@gmail.com>
2026-05-07 18:25:07 -0700

    bugfix: fix overlay on toast notificaton on upload

--- website-generator-app/frontend/src/components/ui/toaster.tsx
+++ website-generator-app/frontend/src/components/ui/toaster.tsx
@@ -9,7 +9,7 @@ export const Toaster: React.FC = () => {
   const { toasts, removeToast } = useToastStore();
 
   return (
-    <div className="fixed bottom-4 right-4 z-50 flex flex-col gap-3 pointer-events-none">
+    <div className="fixed bottom-4 right-4 z-[200] flex flex-col gap-3 pointer-events-none">
       <AnimatePresence mode="popLayout">
         {toasts.map((toast) => (
           <div key={toast.id} className="pointer-events-auto">
